5db39903935f965f79c4d6058b578772eb6cdb51
Hazel Pumphrey <hazel.pumphrey@proton.me>
2024-12-18 15:37:05 +0000

    Added Files

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Hazel Flake Setup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = false;
+    };
+  in {
+    nixosConfigurations = {
+      ms1-hzl4 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs outputs;};
+        modules = [./configuration.nix];
+      };
+      
+      #TODO: Add:
+      #dt1-hzl4 FOR LATER - ONCE THIS IS A GIT REPO
+    };
+  };
+}
