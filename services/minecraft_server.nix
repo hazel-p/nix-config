eb9822b109303876abbc7ae1e010dfb3dc863877
Hazel Pumphrey <eeyjp3@nottingham.ac.uk>
2025-01-03 18:56:59 +0000

    Update minecraft_server.nix

--- services/minecraft_server.nix
+++ services/minecraft_server.nix
@@ -9,16 +9,16 @@
     package = pkgs.papermcServers.papermc-1_20_6;
  
     serverProperties = {
-      gamemode = “survival”;
-      difficulty = “normal”;
+      gamemode = "survival";
+      difficulty = "normal";
       simulation-distance = 10;
-      level-seed = “hideandsoc”;
+      level-seed = "hideandsoc";
     };
  
     whitelist = {
-      ShoQ_ = “a7bbac4b-d6f2-425e-a984-82f092b13da3”;
+      ShoQ_ = "a7bbac4b-d6f2-425e-a984-82f092b13da3";
     };
  
-    jvmOpts = “-Xms4092M -Xmx4092M -XX:+UseG1GC”;
+    jvmOpts = "-Xms4092M -Xmx4092M -XX:+UseG1GC";
   };
 }
