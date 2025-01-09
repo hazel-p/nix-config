65ebdb9aa798cf733135fb0cedaec506902cd25a
Hazel Pumphrey <hazel.pumphrey@proton.me>
2025-01-09 18:47:21 +0000

    added bootloader to config

--- machines/dt1-hzl4/configuration.nix
+++ machines/dt1-hzl4/configuration.nix
@@ -10,6 +10,10 @@
       ./../../modules/nixos/base.nix
     ];
 
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/nvme0n1";
+  boot.loader.grub.useOSProber = true;
+
   networking.hostName = "dt1-hzl4";
 
   # Enable the X11 windowing system.
