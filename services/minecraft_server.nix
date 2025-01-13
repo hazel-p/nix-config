9542e3c5e6e2640cf4fb3e0cde58e108c03e4de2
Hazel Pumphrey <hazel.pumphrey@proton.me>
2025-01-13 01:31:13 +0000

    working server

--- services/minecraft_server.nix
+++ services/minecraft_server.nix
@@ -7,16 +7,18 @@
   services.minecraft-servers = {
     enable = true;
     eula = true;
+    dataDir = "/var/lib/minecraft";
     
     servers = {
       hide-and-soc = {
         enable = true;
         package = pkgs.fabricServers.fabric-1_21_1;
         openFirewall = true;
- 
+        autoStart = true;
+
         serverProperties = {
           server-port = 25565;
-          gamemode = "survival";
+          gamemode = "creative";
           difficulty = "normal";
           simulation-distance = 10;
           level-seed = "hideandsoc";
@@ -58,22 +60,21 @@
                 sha512 = "sha512-YGunjPfzDZnkF8lqoEL2AMG2Ju2ceDkZSW0TneZQAT8UNPz5NUV4LjiJZgMig3zm6FUw2eGlzCD5rRYTV+3kPg==";
               };
 
-            /*
-            # Terrain Generators
-              Tectonic = pkgs.fetchurl {
-                url = "https://cdn.modrinth.com/data/lWDHr9jE/versions/mSYrCaov/tectonic-fabric-1.21.1-2.4.1a.jar"; # 1.21.1
-                sha512 = "sha512-qd2k6xkSpyTh7/ZMpwvp8WX5dD6TbccDxC6+LstM/Rpmpowiqv7wipXIMs6ezPfPg2jG5FV1b5pQV1Ug3UgZGw==";
+            # Fabric API
+              FabricAPI = pkgs.fetchurl {
+                url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/biIRIp2X/fabric-api-0.114.0%2B1.21.1.jar"; # 1.21.1
+                sha512 = "sha512-cUzi6K09TKcASa53H3F5TFATiveAD9wU0rrGsu9jKse6Ownvx446xCXtIOohftSYJCB5LY/nN8s/SdA1OvivBQ==";
               };
-            /*
+
+            # Terrain Generator
               Terralith = pkgs.fetchurl {
                 url = "https://cdn.modrinth.com/data/8oi3bsk5/versions/lQreFvOm/Terralith_1.21.x_v2.5.7.jar"; # 1.21.x
                 sha512 = "sha512-Q9QL/o3OYDt8nr63LbOJ4nfNMFVBRik1DyiDpsdkcqiHrDUKldnsVcKK7BZd7nc2QEYstnTNxMPvswCZ80Y7cg==";
               };
-            */
           });
         };
 
-        jvmOpts = "-Xms2048M -Xmx2048M -XX:+UseG1GC";
+        jvmOpts = "-Xms4096M -Xmx4096M -XX:+UseG1GC";
       };
 
     };
