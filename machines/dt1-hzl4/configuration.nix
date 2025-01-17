cd1e0e3bb81e2602b759df67b356747f825013c9
Hazel Pumphrey <45857842+hzl4@users.noreply.github.com>
2025-01-17 13:56:08 +0000

    Update configuration.nix

--- machines/dt1-hzl4/configuration.nix
+++ machines/dt1-hzl4/configuration.nix
@@ -11,9 +11,8 @@
       ./../../modules/nixos/desktop.nix
     ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/nvme0n1";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   home-manager = {
     useGlobalPkgs = true;
