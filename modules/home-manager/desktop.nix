569e547fa4f117bde5bd98d254ff7fc543581e9e
Hazel Pumphrey <hazel.pumphrey@proton.me>
2025-06-15 13:04:34 +0100

    added gnome config to seperate file

--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{inputs,lib,pkgs, ...}: {
+
+  # Import used desktop configuration
+  imports = [./gnome.nix];
+  
+  # Import used desktop apps
   programs.firefox.enable = true;
   home.packages = with pkgs; [
     spotify
@@ -11,76 +16,5 @@
     texstudio
     texlive.combined.scheme-full
     pandoc
-    # /*
-    gnomeExtensions.alphabetical-app-grid
-    gnomeExtensions.appindicator
-    gnomeExtensions.blur-my-shell
-    gnomeExtensions.clipboard-indicator
-    gnomeExtensions.just-perfection
-    #*/
   ];
-  /*
-  wayland.windowManager.hyprland.enable = true; # enable Hyprland
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    bind =
-      [
-        "$mod, F, exec, firefox"
-        ", Print, exec, grimblast copy area"
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-        builtins.concatLists (builtins.genList (i:
-            let ws = i + 1;
-            in [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-            ]
-          )
-          9)
-      );
-  };
-  #
-  */
-  dconf = {
-    enable = true;
-    settings = {
-      "org/gnome/shell" = {
-        favorite-apps = [
-          "firefox.desktop"
-          "kitty.desktop"
-          "spotify.desktop"
-          "steam.desktop"
-          "stremio.desktop"
-          "discord.desktop"
-          "obsidian.desktop"
-          "prismlauncher.desktop"
-        ];
-      };
-      "org/gnome/desktop/interface" = {
-        color-scheme = "prefer-dark";
-      };
-      "org/gnome/mutter" = {
-        experimental-features = ["scale-monitor-framebuffer"];
-      };
-      "org/gnome/desktop/background" = {
-        picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-d.svg";
-      };
-      "org/gnome/shell" = {
-        disable-user-extensions = false;
-        enabled-extensions = [
-          "AlphabeticalAppGrid@stuarthayhurst"
-          "appindicatorsupport@rgcjonas.gmail.com"
-          "blur-my-shell@aunetx"
-          "clipboard-indicator@tudmotu.com"
-          "just-perfection-desktop@just-perfection"
-        ];
-      };
-      "org/gnome/desktop/interface" = {
-        clock-show-seconds = true;
-      };
-    };
-  };
-  #*/
 }
---
+++ modules/home-manager/gnome.nix
@@ -0,0 +1,50 @@
+{pkgs, ...}: {
+
+  home.packages = with pkgs; [
+    gnomeExtensions.alphabetical-app-grid
+    gnomeExtensions.appindicator
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.clipboard-indicator
+    gnomeExtensions.just-perfection
+  ];
+  
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/shell" = {
+        favorite-apps = [
+          "firefox.desktop"
+          "kitty.desktop"
+          "spotify.desktop"
+          "steam.desktop"
+          "stremio.desktop"
+          "discord.desktop"
+          "obsidian.desktop"
+          "prismlauncher.desktop"
+        ];
+      };
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+      "org/gnome/mutter" = {
+        experimental-features = ["scale-monitor-framebuffer"];
+      };
+      "org/gnome/desktop/background" = {
+        picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-d.svg";
+      };
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = [
+          "AlphabeticalAppGrid@stuarthayhurst"
+          "appindicatorsupport@rgcjonas.gmail.com"
+          "blur-my-shell@aunetx"
+          "clipboard-indicator@tudmotu.com"
+          "just-perfection-desktop@just-perfection"
+        ];
+      };
+      "org/gnome/desktop/interface" = {
+        clock-show-seconds = true;
+      };
+    };
+  };
+}
