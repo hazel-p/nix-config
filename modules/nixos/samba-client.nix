0eb4a90362ffab91a200b9a59bf9e6e8a4025992
Hazel Pumphrey <hazel.pumphrey@proton.me>
2025-11-03 23:28:07 +0000

    fully updated samba - need to add client passwd

--- modules/nixos/samba-client.nix
+++ modules/nixos/samba-client.nix
@@ -11,7 +11,7 @@
     options = let
       # this line prevents hanging on network split
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},user=samba-user,password=${config.sops.secrets."samba-password".path}"];
+    in ["${automount_opts},user=samba-user,password=wromg"];
   };
 
   services.gvfs.enable = true;
