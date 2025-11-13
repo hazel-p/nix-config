1b4ce737f8e6122b6756ffd74ccb08409e5c211a
Hazel Pumphrey <hazel.pumphrey@proton.me>
2025-11-13 20:50:53 +0000

    added more user security to samba client

--- modules/nixos/samba-client.nix
+++ modules/nixos/samba-client.nix
@@ -16,7 +16,7 @@
     options = let
       # this line prevents hanging on network split
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},user=hazel,password=${config.sops.secrets."samba-password".path}"];
+    in ["${automount_opts},user=hazel,password=${config.sops.secrets."samba-password".path},uid=1000,gid=100"];
   };
 
   services.gvfs.enable = true;
