4ef7ad3bd5bd069427ed4b65000bbb3b7dfe88dd
Hazel Pumphrey <hazel.pumphrey@proton.me>
2025-11-18 15:50:02 +0000

    fixed up wayland organisation

--- home/hazel/desktop/wayland-wm/default.nix
+++ home/hazel/desktop/wayland-wm/default.nix
@@ -6,22 +6,12 @@
 }: {
   # Import used desktop configuration
   imports = [
-    ./waybar
+    ./waybar.nix
+    ./rofi.nix
+    ./swww.nix
   ];
 
   # Import wayland apps
-  programs.firefox.enable = true;
   home.packages = with pkgs; [
-    spotify
-    reaper
-    discord
-    stremio
-    prismlauncher
-    obsidian
-    libreoffice-qt
-    hunspell
-    texstudio
-    texlive.combined.scheme-full
-    pandoc
   ];
 }
