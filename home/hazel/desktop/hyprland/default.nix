b700f83eb265ab92d0e8b75304b13e6f74b46892
Hazel Pumphrey <hazel.pumphrey@proton.me>
2025-11-18 16:12:27 +0000

    moved wayland-specific modules into wayland-wm

--- home/hazel/desktop/hyprland/default.nix
+++ home/hazel/desktop/hyprland/default.nix
@@ -25,23 +25,7 @@
     aquamarine # lightweight linux rendering backend library
     hyprgraphics # library providing shared implementations of utilities related to graphics and resources, like loading images or color calculations
     hyprland-qtutils # small bunch of utility applications that hyprland might invoke (stuffs like dialogs and popups)
-
-    swaynotificationcenter # must have notification daemon for smooth hyprland experience (includes pipewire, XDG desktop portal, Authentication agent, Qt wayland support)
-    eww # compositor independent to create widgets (alternative:fabric) , I prefer over waybar
-    clipse # clipboard manager, stores both text and images, can display in a box (alternative:cliphist)
-    wl-clip-persist # solves the problem of copied data getting deleted from clipboard upon closing of applications
-    wl-clipboard # required for clipboards in wayland , cli utility for wayland
-    udiskie # mounts mass storage devices automatically
-    overskride # powerful bluetoothclient in gtk4
-    ashell # minimal alternative to nm-applet(wayland native), simple independent window popup, doesn't require a tray
-    playerctl # cli tool to control media players
-    wldash # a wayland native launcher
-    wlogout # manages logout options, exists independently of hyprlock
-    fira-code # font to be used
-    nwg-look # GTK settings editor, designed to work properly in wlroots-based Wayland environment
   ];
-
-  services.swww.enable = true;
 
   # Environment Variables
   home.sessionVariables = {
--- home/hazel/desktop/wayland-wm/default.nix
+++ home/hazel/desktop/wayland-wm/default.nix
@@ -13,5 +13,18 @@
 
   # Import wayland apps
   home.packages = with pkgs; [
+                swaynotificationcenter # must have notification daemon for smooth hyprland experience (includes pipewire, XDG desktop portal, Authentication agent, Qt wayland support)
+    eww # compositor independent to create widgets (alternative:fabric) , I prefer over waybar
+    clipse # clipboard manager, stores both text and images, can display in a box (alternative:cliphist)
+    wl-clip-persist # solves the problem of copied data getting deleted from clipboard upon closing of applications
+    wl-clipboard # required for clipboards in wayland , cli utility for wayland
+    udiskie # mounts mass storage devices automatically
+    overskride # powerful bluetoothclient in gtk4
+    ashell # minimal alternative to nm-applet(wayland native), simple independent window popup, doesn't require a tray
+    playerctl # cli tool to control media players
+    wldash # a wayland native launcher
+    wlogout # manages logout options, exists independently of hyprlock
+    fira-code # font to be used
+    nwg-look # GTK settings editor, designed to work properly in wlroots-based Wayland environment
   ];
 }
