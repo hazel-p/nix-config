a772fd6585f80775b051625b57811a901d787302
Hazel Pumphrey <hazel.pumphrey@proton.me>
2025-11-28 18:17:40 +0000

    removed duplicates

--- nixos/base.nix
+++ nixos/base.nix
@@ -101,24 +101,5 @@
     networkmanager.enable = true;
   };
 
-  environment.persistence."/nix/persist" = {
-    # Hide these mounts from the sidebar of file managers
-    hideMounts = true;
-
-    directories = [
-      "/var/log"
-      # inspo: https://github.com/nix-community/impermanence/issues/178
-      "/var/lib/nixos"
-    ];
-
-    files = [
-      "/etc/machine-id"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
-    ];
-  };
-
   system.stateVersion = "24.11"; #Don't change
 }
