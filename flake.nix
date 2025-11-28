caa17e69f4361396f01ef93cd45c427419b684cc
Hazel Pumphrey <hazel.pumphrey@proton.me>
2025-11-28 18:08:41 +0000

    changed impermanence flake source

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
 
     impermanence = {
-      url = "github:misterio77/impermanence";
+      url = "github:nix-community/impermanence";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
