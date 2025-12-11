bad5ca7790b11e23ac1f37e9db930d65c4fd7a74
Hazel Pumphrey <hazel.pumphrey@proton.me>
2025-12-11 11:54:45 +0000

    finished fastfetch rice for now, might change colours later

--- hazel-home/cli/fastfetch.nix
+++ hazel-home/cli/fastfetch.nix
@@ -13,64 +13,153 @@
           type = "custom";
           format = "┌───────────────────── Hardware ─────────────────────┐";
         }
-        "host"
+        {
+          type = "host";
+          key = "  host";
+          keyColor = "red";
+        }
         {
           type = "cpu";
+          key = "  cpu";
+          keyColor = "red";
           temp = true;
           format = "{name} ({cores-physical}/{cores-logical} @{freq-max:4}) {temperature}";
         }
         {
           type = "gpu";
+          key = "  gpu";
+          keyColor = "red";
           temp = true;
           format = "{vendor} {name} ({12}) {temperature}";
         }
-        "memory"
-        "disk"
-        "battery"
-        "poweradapter"
+        {
+          type = "memory";
+          key = "  mem";
+          keyColor = "red";
+        }
+        {
+          type = "disk";
+          key = "  disk [/{name}]";
+          keyColor = "red";
+        }
+        {
+          type = "battery";
+          key = "  battery";
+          keyColor = "red";
+        }
+        {
+          type = "poweradapter";
+          key = "  power";
+          keyColor = "red";
+        }
         {
           type = "custom";
           format = "└────────────────────────────────────────────────────┘";
         }
-
-        "break"
 
         {
           type = "custom";
           format = "┌───────────────────── Software ─────────────────────┐";
         }
-        "os"
-        "kernel"
-        "packages"
-        "shell"
-
-        "de"
-        "monitor"
-        "wm"
-        "wmtheme"
-        "theme"
-        "icons"
-        "font"
-        "cursor"
+        {
+          type = "os";
+          key = "  os";
+          keyColor = "yellow";
+        }
+        {
+          type = "kernel";
+          key = "  kernel";
+          keyColor = "yellow";
+        }
+        {
+          type = "packages";
+          key = "  packages";
+          keyColor = "yellow";
+        }
+        {
+          type = "shell";
+          key =  "  shell";
+          keyColor = "yellow";
+        }
+        {
+          type = "de";
+          key = "  de";
+          keyColor = "blue";
+        }
+        {
+          type = "monitor";
+          key = "  monitor";
+          keyColor = "blue";
+        }
+        {
+          type = "wm";
+          key = "  wm";
+          keyColor = "blue";
+        }
+        {
+          type = "wmtheme";
+          key = "  wmtheme";
+          keyColor = "blue";
+        }
+        {
+          type = "theme";
+          key = "  theme";
+          keyColor = "blue";
+        }
+        {
+          type = "icons";
+          key = "  icons";
+          keyColor = "blue";
+        }
+        {
+          type = "font";
+          key = "  font";
+          keyColor = "blue";
+        }
+        {
+          type = "cursor";
+          key = "  cursor";
+          keyColor = "blue";
+        }
         {
           type = "custom";
           format = "└────────────────────────────────────────────────────┘";
         }
-        "break"
+
         {
           type = "custom";
           format = "┌─────────────── Configuration & Info ───────────────┐";
         }
-        "uptime"
-        "datetime"
-        "localip"
-        "locale"
+        {
+          type = "uptime";
+          key = "  uptime";
+          keyColor = "green";
+        }
+        {
+          type = "datetime";
+          key = "  date & time";
+          keyColor = "green";
+        }
+        {
+          type = "localip";
+          key = "  local ip";
+          keyColor = "green";
+        }
+        {
+          type = "locale";
+          key = "  locale";
+          keyColor = "green";
+        }
         {
           type = "custom";
           format = "└────────────────────────────────────────────────────┘";
         }
-        "break"
-        "colors"
+
+        {
+          type = "colors";
+          symbol = "circle";
+          paddingLeft = 19;
+        }
       ];
     };
   };
